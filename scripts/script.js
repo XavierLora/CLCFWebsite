a9e756d64a14e649709e98d5bd06ce36172bb62c
Xavier Lora <XavierLora0727@gmail.com>
2023-12-13 12:53:35 -0500

    Fixed Nav to intersect with Image

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,17 @@
-const navbar = document.getElementById("navbarId");
+var navbar = document.getElementById("navbarId");
+var image = document.getElementById("teamImg");
 
-            let lastScrollTop = 0;
-            addEventListener("scroll", () => {
-                // Current scroll position
-                const scrollTop =
-                    window.pageYOffset || document.documentElement.scrollTop;
+var observer = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      // Image is in view
+      navbar.style.top = "0";
+    } else {
+      // Image is out of view
+      navbar.style.top = "-50vw"; // Adjust this value as needed
+    }
+  });
+}, { threshold: 0.25 }); // Adjust the threshold as needed
 
-                // check scroll direction
-                const distance = scrollTop - lastScrollTop;
-                const currentTop = parseInt(
-                    getComputedStyle(navbar).top.split("px")
-                );
-
-                // Clamp value between -80 and 0
-                let amount = Math.max(
-                    Math.min(
-                        currentTop +
-                            (distance < 0
-                                ? Math.abs(distance) // scrolling up
-                                : -Math.abs(distance)// scrolling down
-                                ) * 40, // (1)
-                        0
-                    ),
-                    -80
-                );
-
-                console.log(amount, currentTop, Math.abs(distance));
-
-                navbar.style.top = `${amount*1.9}px`;
-
-                lastScrollTop = scrollTop;
-            });
+// Start observing the image
+observer.observe(image);
