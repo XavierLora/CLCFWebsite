4f0552d55e528b9570c99f1b1c2ae8c93201de25
Xavier Lora <XavierLora0727@gmail.com>
2023-12-20 16:50:41 -0500

    Same issue as before

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 var navbar = document.getElementById("navbarId");
 var image = document.getElementById("teamImg");
-var imageCropped = document.getElementById("teamImgCrop");
+
 
 var observer = new IntersectionObserver(function (entries) {
   entries.forEach(function (entry) {
@@ -16,18 +16,3 @@ var observer = new IntersectionObserver(function (entries) {
 
 // Start observing the image
 observer.observe(image);
-
-var observer = new IntersectionObserver(function (entries) {
-  entries.forEach(function (entry) {
-    if (entry.isIntersecting) {
-      // Image is in view
-      navbar.style.top = "0";
-    } else {
-      // Image is out of view
-      navbar.style.top = "-30vw"; // Adjust this value as needed
-    }
-  });
-}, { threshold: 0.01 }); // Adjust the threshold as needed
-
-// Start observing the image
-observer.observe(imageCropped);
